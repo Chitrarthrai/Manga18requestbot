add58b0f1893cf77ac38527394f35240d666300b
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-08-23 11:47:21 +0530

    0.0.5

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -5,8 +5,10 @@ from telethon.sessions import StringSession
 API_ID = int(os.environ.get('API_ID'))
 API_HASH = os.environ.get('API_HASH')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-STRING_SESSION = os.environ.get('STRING_SESSION')
+SESSION = os.environ.get('STRING')
+if SESSION:
+  print('Session loaded..')
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
-scraper = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH)
+scraper = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
