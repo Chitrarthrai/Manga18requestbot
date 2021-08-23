01ec3ed38ccbe3e335eb52ea97e0f7c778110119
Zack-Bloodshot <zackbloodshot@gmail.com>
2021-08-23 11:35:23 +0530

    0.0.3

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -5,7 +5,7 @@ from telethon.sessions import StringSession
 API_ID = int(os.environ.get('API_ID'))
 API_HASH = os.environ.get('API_HASH')
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
-STRING_SESSION = os.enviorn.get('STRING_SESSION')
+STRING_SESSION = os.environ.get('STRING_SESSION')
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
